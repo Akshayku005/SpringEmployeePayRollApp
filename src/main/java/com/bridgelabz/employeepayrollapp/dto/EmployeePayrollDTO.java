d1cb6be2175afd7600f821afa637cb1a9e457618
Akshayku005 <akshaysportive@gmail.com>
2022-05-24 08:53:57 +0530

    added: Lombok

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -13,29 +13,5 @@ public class EmployeePayrollDTO {
         this.name = name;
         this.salary = salary;
     }
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
 }
 
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.employeepayrollapp.model;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import lombok.Data;
 
 
-
-public class EmployeePayrollData {
+public @Data class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
 
-    public EmployeePayrollData() {
+    public  EmployeePayrollData() {
 
     }
 
@@ -17,29 +17,5 @@ public class EmployeePayrollData {
         this.employeeId = empId;
         this.name = empPayrollDTO.name;
         this.salary = empPayrollDTO.salary;
-    }
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
     }
 }
